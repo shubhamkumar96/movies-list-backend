54772248f8e031deb9770bce5b1076727d6a2474
shubhamkumar96 <shubhamkumarranu@gmail.com>
2023-10-02 07:45:16 +0530

    Added Review Controller

---
+++ src/main/java/dev/project/movies/ReviewService.java
@@ -0,0 +1,27 @@
+package dev.project.movies;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+    @Autowired
+    private ReviewRepository reviewRepository;
+    //  "MongoTemplate" is another way to talk to a database other than "Repository", which we can use to create new
+    //  complex dynamic Queries, which can't be done, or is not suitable to do inside "Repository".
+    @Autowired
+    private MongoTemplate mongoTemplate;
+    public Review createReview(String reviewBody, String imdbId) {
+        Review review = reviewRepository.insert(new Review(reviewBody));
+
+        mongoTemplate.update(Movie.class)
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviewIds").value(review))
+                .first();
+
+        return review;
+    }
+}
